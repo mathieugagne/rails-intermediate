9a49407cb8dcab3fcdc6f9edab9ff044ed6c4649
Mathieu Gagne <gagne.mathieu@hotmail.com>
2014-04-29 20:45:19 -0400

    Adds last steps

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,10 @@ Rails.application.routes.draw do
   get '/setup-users', to: 'steps#setup_users'
   get '/add-authentication-forms-and-links', to: 'steps#add_authentication_forms_and_links'
   get '/create-comments', to: 'steps#create_comments'
+  get '/display-comments', to: 'steps#display_comments'
+  get '/submit-comments', to: 'steps#submit_comments'
   get '/add-projects-workflow', to: 'steps#add_projects_workflow'
-  get '/sort-projects', to: 'steps#sort_projects'
+  get '/next-steps', to: 'steps#next_steps'
 
   root to: 'steps#overview'
 end
